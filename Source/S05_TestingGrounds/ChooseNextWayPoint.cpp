1009931977e91d7109836a303ccbca84330cd406
Ludescher <ludescher@dp-graphics.ch>
2018-01-06 23:03:45 +0100

    Convert Choose next WayPoint in CPP

--- Source/S05_TestingGrounds/ChooseNextWayPoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWayPoint.cpp
@@ -2,14 +2,42 @@
 
 #include "S05_TestingGrounds.h"
 #include "ChooseNextWayPoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+
 
 
 
 
 EBTNodeResult::Type UChooseNextWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("ExecuteTask running"));
 
+
+	
+
+	//Get the PatrolPoints
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCpp;
+
+	// Set next Waypoint
+	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComponent->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	
+
+	
+	//CycleWaypointIndex
+	int32 NewIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComponent->SetValueAsInt(IndexKey.SelectedKeyName, NewIndex);
+	//TODO Protect against empty patrol route 
+
+	//auto name = AIController->GetPawn()->GetName();
+	//UE_LOG(LogTemp, Warning, TEXT("WaypointIndex = %s"), *name);
+	
 
 	return EBTNodeResult::Succeeded;
 
--- Source/S05_TestingGrounds/ChooseNextWayPoint.h
+++ Source/S05_TestingGrounds/ChooseNextWayPoint.h
@@ -15,5 +15,10 @@ class S05_TESTINGGROUNDS_API UChooseNextWayPoint : public UBTTaskNode
 	
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector IndexKey; 
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
